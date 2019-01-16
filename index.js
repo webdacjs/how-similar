cb62fdd05a619b2e3025e33eb54fa1953b30fc01
webdac <webdac@gmail.com>
2019-01-16 07:27:30 +0100

    Adding similarity generic function and check arguments function

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const {getMaxLen, getDistance} = require('./utils')
+const {getMaxLen, getDistance, checkArgs} = require('./utils')
 const soa = require('sort-objects-array')
 
-const stringSimilarity = (str1, str2, opts) => {
-  const maxlen = getMaxLen([str1, str2], opts)[0].len
-  return getDistance(str1, str2, maxlen, opts)
-}
+const stringSimilarity = (str1, str2, opts) => checkArgs(str1, str2) === 'string'
+  ? getDistance(str1, str2, getMaxLen([str1, str2], opts)[0].len, opts)
+  : 'Both arguments need to be String for stringSimilarity'
 
 const stringArraySimilarity = (str1, arr1, opts) => {
-  const maxlen = getMaxLen([str1, ...arr1], opts)[0].len
-  const similarities = arr1.map(x => (
-    {key: x, value: getDistance(str1, x, maxlen, opts)}))
-  return soa(similarities, 'value', 'desc')
+  if (checkArgs(str1, arr1) === 'array') {
+    const maxlen = getMaxLen([str1, ...arr1], opts)[0].len
+    const similarities = arr1.map(x => (
+      {key: x, value: getDistance(str1, x, maxlen, opts)}))
+    return soa(similarities, 'value', 'desc')
+  }
+  return 'First argument need to be a string a second an array for stringArraySimilarity'
 }
+
+const similarity = (arg1, arg2, opts) => checkArgs(
+  arg1, arg2) === 'string'
+  ? stringSimilarity(arg1, arg2, opts)
+  : stringArraySimilarity(arg1, arg2, opts)
 
 module.exports = {
   stringSimilarity,
-  stringArraySimilarity
+  stringArraySimilarity,
+  similarity
 }
